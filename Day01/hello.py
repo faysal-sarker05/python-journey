cd487dc7c60cb37c8a9f582a6a9c8eeafaae78d3
Faysal Sarker <faysalsarker05.edu@gmail.com>
2026-07-09 01:14:14 +0600

    Add Day 1 Python introduction program

---
+++ Day01/hello.py
@@ -0,0 +1,9 @@
+print("=" * 40)
+print("Welcome to My Python Journey!")
+print("=" * 40)
+
+name = "Faysal Sarker"
+
+print("Name:", name)
+print("Goal: Become an AI and Robotics Engineer")
+print("Today's Lesson: Day 1")
