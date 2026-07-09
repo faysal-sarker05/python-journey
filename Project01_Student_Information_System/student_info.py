5367367bfceff70395b1e41728da628379bb0adb
Faysal Sarker <faysalsarker05.edu@gmail.com>
2026-07-09 20:01:17 +0600

    Complete Profect 1: StudentInformation System v1

---
+++ Project01_Student_Information_System/student_info.py
@@ -0,0 +1,19 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+department = input("Enter your department: ")
+cgpa = float(input("Enter your CGPA: "))
+
+print("=" * 40)
+
+print("     Student Information System")
+
+print("=" * 40)
+
+print("Name         :", name)
+print("Age          :", age)
+print("Department   :", department)
+print("CGPA         :", cgpa)
+
+print("=" * 40)
+print("Thank you for using the program!")
+print("=" * 40)
