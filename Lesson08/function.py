0977246380064b61fb39917e85552c3d4d304f77
Faysal Sarker <faysalsarker05.edu@gmail.com>
2026-07-11 02:40:56 +0600

    ATM simulator v3

---
+++ Lesson08/function.py
@@ -0,0 +1,11 @@
+def show_menu():
+    print("=" * 40)
+    print("        ATM Menu")
+    print("=" * 40)
+
+    print("1. Check Balance")
+    print("2. Withdraw")
+    print("3. Deposit")
+    print("4. Exit")
+
+show_menu()
